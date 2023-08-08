485eb18e3890d330acce5fea4a61331fd6399cce
Ben Cavins <ben.cavins@flatironschool.com>
2023-08-08 15:23:12 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Space Shooter
+
+## Local Dev
+
+### Need to download a web server
+For Mac I use https://www.mamp.info/en/downloads/
+
+For Windows, can use https://sourceforge.net/projects/wampserver/
+
---
+++ game.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    main()
+}
+
+function main() {
+    let config = {
+        width: 800,
+        height: 600,
+        backgroundColor: 0x000000
+    }
+    let game = new Phaser.Game(config)
+}
